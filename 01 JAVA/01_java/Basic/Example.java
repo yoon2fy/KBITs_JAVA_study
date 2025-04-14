273ad8d1d9ba1bf78e8cce2a0048c3adc0acfb37
JiYoon Yang <yoon2fy@gmail.com>
2025-04-14 18:51:24 +0900

    01_java(기본)
    01_java(기본)

---
+++ 01 JAVA/01_java/Basic/Example.java
@@ -0,0 +1,14 @@
+package ch01.verify;
+/**
+ * @author JiYoon Yang
+ */
+/*
+장제목: 1장 자바 시작하기
+작성일: 2025.04.14
+*/
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("개발자가 되기 위한 필수 개발 언어 Java");
+    }
+}
