45ea2b956e0d3b3576424c42f434c2724e7c9f62
JiYoon Yang <yoon2fy@gmail.com>
2025-04-17 19:48:40 +0900

    03_java(기본)
    03_java(기본)

---
+++ 01 JAVA/03_java/Basic/ch06.sec06.exam01/Car.java
@@ -0,0 +1,14 @@
+package ch06.sec06.exam01;
+/**
+ * @author JiYoon Yang
+ */
+/*
+장제목: 6장 클래스 > 기본문제
+[문제] 다음과 같이 Car 클래스를 정의한 경우, 각 멤버 변수가 가지는 기본값은 무엇인가?
+작성일: 2025.04.17
+*/
+public class Car {
+    String model;
+    boolean start;
+    int speed;
+}
