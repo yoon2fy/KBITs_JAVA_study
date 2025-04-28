72c643dd5503c068120a8bef41648462aff4b0b8
JiYoon Yang <yoon2fy@gmail.com>
2025-04-29 08:14:52 +0900

    14_java(복습)
    14_java(복습)

---
+++ 02_Learning/Ch14_multiThread/examples/BeepPrintExample.java
@@ -0,0 +1,15 @@
+package ch14.sec03.exam01;
+import java.awt.Toolkit;
+public class BeepPrintExample {
+    public static void main(String[] args) {
+        Toolkit toolkit = Toolkit.getDefaultToolkit(); // Toolkit 객체 얻기
+        for (int i = 0; i < 5; i++) {
+            toolkit.beep(); // 비프음 발생
+            try {Thread.sleep(500);} catch (Exception e) {} // 0.5초간 일시 정지
+        }
+        for (int i = 0; i < 5; i++) {
+            System.out.println("띵");
+            try {Thread.sleep(500);} catch (Exception e) {} // 0.5초간 일시 정지
+        }
+    }
+}
