1beb8fe2c8e98123ea08c34bb424579bc17df861
JiYoon Yang <yoon2fy@gmail.com>
2025-05-07 18:06:17 +0900

    04_java(심화)
    04_java(심화)

---
+++ 01 JAVA/04_java/Deep/AbstractMethodExample.java
@@ -0,0 +1,17 @@
+package Deep;
+
+public class AbstractMethodExample {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.sound();
+
+        Cat cat = new Cat();
+        cat.sound();
+
+        animalSound(new Dog());
+        animalSound(new Cat());
+    }
+    public static void animalSound(Animal animal){
+        animal.sound(); // 재정의된 메소드 호출
+    }
+}
