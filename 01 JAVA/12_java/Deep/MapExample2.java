7708ee1df881a8025dd97e61f7c27ab64f9def75
JiYoon Yang <yoon2fy@gmail.com>
2025-05-08 17:25:54 +0900

    12_java(심화)
    12_java(심화)

---
+++ 01 JAVA/12_java/Deep/MapExample2.java
@@ -0,0 +1,21 @@
+package Deep;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class MapExample2 {
+    public static void main(String[] args) {
+        int[] intArray = {1, 2, 3, 4, 5};
+
+        IntStream intStream = Arrays.stream(intArray);
+        intStream
+                .asDoubleStream()
+                .forEach(d -> System.out.println(d));
+        System.out.println();
+
+        intStream = Arrays.stream(intArray);
+        intStream
+                .boxed()
+                .forEach(obj -> System.out.println(obj.intValue()));
+    }
+}
