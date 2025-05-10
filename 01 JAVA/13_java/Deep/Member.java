d3c7d2fe9c9b8a5e6332f4d8cd255728f9244830
JiYoon Yang <yoon2fy@gmail.com>
2025-05-10 16:08:13 +0900

    13_java(심화)
    13_java(심화)

---
+++ 01 JAVA/13_java/Deep/Member.java
@@ -0,0 +1,18 @@
+package Deep;
+
+import java.io.Serializable;
+
+public class Member implements Serializable {
+    private static final long serialVersionUID = -622284561026719240L;
+    private String id;
+    private String name;
+
+    public Member(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return id + ": " + name;
+    }
+}
